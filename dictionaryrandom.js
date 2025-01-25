d8b7669a273f396f2ac370df0ee5c90767655c5f
duongcoder211 <daomanhduong02112005@gmail.com>
2025-01-26 02:23:13 +0700

    Update dictionaryrandom.js

--- dictionaryrandom.js
+++ dictionaryrandom.js
@@ -1,2 +1,4 @@
 export let stringInputExport;
-stringInputExport = "";
+stringInputExport = "Thẩm phán-Cудья;Tòa án-Суд;Luật sư của tòa án-Адвокат;Tiền boa-Чаевые деньги;Luật học-Юриспруденция;Cải thiện,nâng cao-Улучшать,улучшить;
+  Cuộc thi-Соревнование;
+";
