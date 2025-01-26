bc2040c2ddaa749a0830decee53667fe56898795
duongcoder211 <daomanhduong02112005@gmail.com>
2025-01-26 14:01:05 +0700

    Delete dictionaryrandom.js

--- dictionaryrandom.js
+++
@@ -1,3 +0,0 @@
-export let stringInputExport;
-stringInputExport = `Thẩm phán-Cудья;Tòa án-Суд;Luật sư của tòa án-Адвокат;Tiền boa-Чаевые деньги;Luật học-Юриспруденция;Cải thiện, nâng cao-Улучшать, улучшить;
-Cuộc thi-Соревнование;`;
